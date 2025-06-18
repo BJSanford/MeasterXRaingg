04e57293035ef030e5183d8ce04237b5a4f4f853
BJSanford <80666452+BJSanford@users.noreply.github.com>
2025-06-18 00:01:04 +0000

    ohyeah

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,11 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
-import { verify } from "cookie-signature";
 
 export async function middleware(request: NextRequest) {
-  const discordUsername = request.cookies.get("discordUsername")?.value
-  const rainUsernameCookie = request.cookies.get("rainUsername")?.value;
-  const rainIdCookie = request.cookies.get("rainId")?.value;
-  const verified = request.cookies.get("verified")?.value
-
-  // Validate signed cookies
-  const rainUsername = rainUsernameCookie && verify(rainUsernameCookie, process.env.COOKIE_SECRET);
-  const rainId = rainIdCookie && verify(rainIdCookie, process.env.COOKIE_SECRET);
+  const discordUsername = request.cookies.get("discordUsername")?.value;
+  const rainUsername = request.cookies.get("rainUsername")?.value;
+  const rainId = request.cookies.get("rainId")?.value;
+  const verified = request.cookies.get("verified")?.value;
 
   // Ensure proper redirection to dashboard
   if (
@@ -18,15 +13,15 @@ export async function middleware(request: NextRequest) {
     (!discordUsername || !rainUsername || verified !== "true" || !rainId)
   ) {
     if (!discordUsername || !rainUsername) {
-      return NextResponse.redirect(new URL("/login", request.url))
+      return NextResponse.redirect(new URL("/login", request.url));
     }
 
     if (verified !== "true") {
-      return NextResponse.redirect(new URL("/verification-pending", request.url))
+      return NextResponse.redirect(new URL("/verification-pending", request.url));
     }
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
